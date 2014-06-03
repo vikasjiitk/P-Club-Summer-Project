9166a9f95208569688a379d44311d55d5215f22d
pcp10 <2014pcp10@gmail.com>
2014-06-03 13:50:19 +0530

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,17 +1,18 @@
+
 <!DOCTYPE html> 
 <html> 
 <head>
-	<title>
+ <title>
  	SHARE Ur FARE
  </title>
-	<?php session_start();
-	if(!$_SESSION['loggedin'])
-	{
-		header("Location:login.php");
-		exit;
-	}
-	?>
-   <style type="text/css"> 
+ 	<?php session_start();
+if(!$_SESSION['loggedin'])
+{
+header("Location:login.php");
+exit;
+}
+?>
+  <style type="text/css"> 
   h1{color:teal;} 
   .id1{ color:goldenrod; font-size: 22px; } 
   .id1:hover{ color:gold; }
@@ -20,7 +21,6 @@
   .id3:hover{ color:yellowgreen; } 
   .id4{ color:mediumslateblue; font-size: 22px; } 
   .id4:hover{ color:gold; } 
-  .id5:{ color:gold font-size: 12px; }
 </style> 
 </head> 
 <body style="background-color:lavender;"> 
@@ -30,15 +30,15 @@
 	<hr>
 	<hr> 
 	<p style="word-spacing: 3em;">
-		<a href="welcome.php" target=_top STYLE="text-decoration: none"><i><b><font class="id3">Home</font></b></i></a> 
+		<a href="" target=_top STYLE="text-decoration: none"><i><b><font class="id3">Home</font></b></i></a>
+		<a href="create_group.php" target=_top STYLE="text-decoration: none"><i><b><font class="id3">CreateGroup</font></b></i></a> 
 		<a href="" target=_top STYLE="text-decoration: none"><i><b><font class="id3">Contacts</font></b></i></a> 
 		<a href="" target=_top STYLE="text-decoration: none"><i><b><font class="id3">Profile</font></b></i></a> 
 		<a href="" target=_top STYLE="text-decoration: none"><i><b><font class="id3">Notifications</font></b></i></a> 
 		<a href="" target=_top STYLE="text-decoration: none"><i><b><font class="id3" style="word-spacing: 0.2em;">About us</font></b></i></a> 
 		<a href="" target=_top STYLE="text-decoration: none"><i><b><font class="id3">Help</font></b></i></a> 
-		<a href="result.php" target=_top STYLE="text-decoration: none"><i><b><font class="id3"style="word-spacing: 0.2em;">Groups</font></b></i></a>
 		<a href="signout.php" target=_top STYLE="text-decoration: none"><i><b><font class="id3">Signout</font></b></i></a></p> <hr> <hr> 
-		<form action="result.php" method ="POST"> 
+		<form action="group_search.php" method="post"> 
 			<font class= "id4">Source: </font>
 			<input type="text" name="source"><br><br> 
 			<font class= "id4">Destination: </font>
@@ -47,29 +47,17 @@
 			<input type="date" name="date"><br><br> 
 			<font class= "id4">Time: </font>
 			<input type="time" name="time"><br><br> 
-			<font class= "id4"><abbr title="Time + Time variation=maximum time extended">Time variation: </abbr>
+			<font class= "id4"><abbr title="Time variation in hours">Time variation: </abbr>
 			</font>
-			<input type="time" name="variation"><br><br> 
-			<font class= "id4">Gender Specific: </font>
-			<input type="radio" name="gender"
-			<?php if (isset($gender) && $gender=="female");?>
-			value="female">Female
-			<input type="radio" name="gender"
-			<?php if (isset($gender) && $gender=="male");?>
-			value="male">Male
-			<input type="radio" name="gender"
-			<?php if (isset($gender) && $gender=="any");?>
-			value="any">Any
-			<br><br>
+			<input type="number" name="variation"><br><br>
+			<font class= "id4"><abbr title="gender specification in sharing">Gender: </font>
+			<input type="radio" name="gender" value="F">only female 
+			<input type="radio" name="gender" value="M">only male 
+			<input type="radio" name="gender" value="B">both <br><br> 
 			<font class= "id4">Vehicle type: </font>
-			<input list="vehicle" name="vehicle">
-			<datalist id="vehicle">
-  			<option value="Auto">
-  			<option value="Vikram">
-  			<option value="Any">
-  			</datalist><br><br> 
+			<input type="text" name="vehicle"><br><br> 
 			<font class= "id4"><abbr title="for whom u r booking">No. of people: </abbr></font>
-			<input type="number" name="number"><br><br> <input type="submit" name="submit" value="Submit data"> 
+			<input type="number" name="number"><br><br> <input type="submit" name="submit" value="Find Group"> 
 		</form> 
 	</body> 
 	</html>

9c9a313be93307b789859e38a0537eab92994161
pcp10 <2014pcp10@gmail.com>
2014-06-07 14:57:04 +0530

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,62 +1,130 @@
-<!DOCTYPE html> 
-<html> 
+<!DOCTYPE html>
+<html>
 <head>
- <title>
- 	SHARE Ur FARE
+<title>
+  SHARE Ur FARE
  </title>
- 	<?php session_start();
+ <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="assets/ico/favicon.ico">
+
+    
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="navbar-fixed-top.css" rel="stylesheet">
+    <link href="welcome.css" rel="stylesheet">
+    
+<?php session_start();
 if(!$_SESSION['loggedin'])
 {
 header("Location:login.php");
 exit;
 }
 ?>
-  <style type="text/css"> 
-  h1{color:teal;} 
-  .id1{ color:goldenrod; font-size: 22px; } 
-  .id1:hover{ color:gold; }
-  .id2:hover{ color:indigo; } 
-  .id3{ color:olivedrab; font-size: 22px; } 
-  .id3:hover{ color:yellowgreen; } 
-  .id4{ color:mediumslateblue; font-size: 22px; } 
-  .id4:hover{ color:gold; } 
-</style> 
-</head> 
-<body style="background-color:lavender;"> 
-	<h1 style="text-align:Center;"><i><b><font class="id2"><ins>SHARE ur FARE</ins></font></b></i></h1> 
-	<hr> 
-	<font class="id1">Disclaimer:</font>
-	<hr>
-	<hr> 
-	<p style="word-spacing: 3em;">
-		<a href="" target=_top STYLE="text-decoration: none"><i><b><font class="id3">Home</font></b></i></a>
-		<a href="create_group.php" target=_top STYLE="text-decoration: none"><i><b><font class="id3">CreateGroup</font></b></i></a> 
-		<a href="" target=_top STYLE="text-decoration: none"><i><b><font class="id3">Contacts</font></b></i></a> 
-		<a href="" target=_top STYLE="text-decoration: none"><i><b><font class="id3">Profile</font></b></i></a> 
-		<a href="" target=_top STYLE="text-decoration: none"><i><b><font class="id3">Notifications</font></b></i></a> 
-		<a href="" target=_top STYLE="text-decoration: none"><i><b><font class="id3" style="word-spacing: 0.2em;">About us</font></b></i></a> 
-		<a href="" target=_top STYLE="text-decoration: none"><i><b><font class="id3">Help</font></b></i></a> 
-		<a href="signout.php" target=_top STYLE="text-decoration: none"><i><b><font class="id3">Signout</font></b></i></a></p> <hr> <hr> 
-		<form action="group_search.php" method="post"> 
-			<font class= "id4">Source: </font>
-			<input type="text" name="source"><br><br> 
-			<font class= "id4">Destination: </font>
-			<input type="text" name="destination"><br><br> 
-			<font class= "id4">Date: </font>
-			<input type="date" name="date"><br><br> 
-			<font class= "id4">Time: </font>
-			<input type="time" name="time"><br><br> 
-			<font class= "id4"><abbr title="Time variation in hours">Time variation: </abbr>
-			</font>
-			<input type="number" name="variation"><br><br>
-			<font class= "id4"><abbr title="gender specification in sharing">Gender: </font>
-			<input type="radio" name="gender" value="F">only female 
-			<input type="radio" name="gender" value="M">only male 
-			<input type="radio" name="gender" value="B">both <br><br> 
-			<font class= "id4">Vehicle type: </font>
-			<input type="text" name="vehicle"><br><br> 
-			<font class= "id4"><abbr title="for whom u r booking">No. of people: </abbr></font>
-			<input type="number" name="number"><br><br> <input type="submit" name="submit" value="Find Group"> 
-		</form> 
-	</body> 
-	</html>
+<style type="text/css">
+h1{ font-family: Magneto;
+	color:teal;}
+	b.red{
+		color:red;
+	}
+	hr{color: red}
+	body {background-image:url("b1.jpg");}
+.id1{ color:goldenrod; font-size: 22px; }
+.id1:hover{ color:gold; }
+.id2:hover{ color:indigo; }
+.id3{ color:olivedrab; font-size: 22px; }
+.id3:hover{ color:yellowgreen; }
+.id4{ color:mediumslateblue; font-size: 15px; }
+.id4:hover{ color:gold; }
+.id5:{ color:gold font-size: 12px; }
+</style>
+</head>
+<body style="background-color:lavender;">
+	<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Welcome <?php echo $_SESSION['userlogin']?> !</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="welcome1.php"><span class="glyphicon glyphicon-home"></span>  Home</a></li>
+            <li><a href="#about"><span class="glyphicon glyphicon-phone-alt"></span>  Contacts</a></li>
+            <li><a href="#contact"><span class="glyphicon glyphicon-user"></span>  Profile</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-th-list"></span> <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Notifications</a></li>
+                <li><a href="#">Help</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Sign-out</a></li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="../navbar/">Help</a></li>
+            <li><a href="../navbar-static-top/">About Us</a></li>
+            <li class="active"><a href="signout.php"><span class="glyphicon glyphicon-log-out"></span>  Sign-out</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+<h1 style="text-align:Center;"><b><font class="id2"><ins>Share Ur Fare</ins></font></b></h1>
+
+<marquee><b class=red>Disclaimer:</b><i>If any person in your group fails to come for the journey,then the site would not be responsible. Hence, user discretion is adviced.</i></marquee>
+
+<h3 class="form-signin-heading">Please fill the following details.</h3>
+<form class="form-signin" action="result.php" role="form" method ="POST">
+
+<input type="text" class="form-control" placeholder="Source" name="source" required autofocus>
+        <br><input type="text" class="form-control" placeholder="Destination" name="destination" required>
+        <br><input type="date"  onblur="(this.type='text')" onfocus="(this.type='date')" class="form-control" placeholder="Date Of Journey" name="date" required>
+        <br><input type="time"  onfocus="(this.type='time')" onblur="(this.type='text')" class="form-control" placeholder="Time" name="time" required>
+        <br><input type="time" type="text" onfocus="(this.type='time')" onblur="(this.type='text')" class="form-control" placeholder="Time Variation" name="variation" required>
+        <br><input class="form-control" placeholder="Type of vehicle" list="vehicle" name="vehicle">
+
+<datalist id="vehicle">
+<option value="Auto">
+<option value="Vikram">
+<option value="Any">
+</datalist>
+ <br><input type="number" class="form-control" placeholder="Number Of People" name="number" required>
+       <br>  <font class= "id4">Gender Specific: </font>
+<input type="radio" name="gender"
+<?php if (isset($gender) && $gender=="female");?>
+value="female">Female
+<input type="radio" name="gender"
+<?php if (isset($gender) && $gender=="male");?>
+value="male">Male
+<input type="radio" name="gender"
+<?php if (isset($gender) && $gender=="any");?>
+value="any">Any
+   <br><br>
+   <button class="btn btn-lg btn-primary btn-block" type="submit">Submit
+</button></form>
+
+
+
+
+
+
+
+
+</body>
+</html>

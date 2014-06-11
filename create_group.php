7fcad2308670a8bb95476b7660f0c5e3b097f042
pcp10 <2014pcp10@gmail.com>
2014-06-11 15:22:41 +0530

    Update create_group.php

--- create_group.php
+++ create_group.php
@@ -1,17 +1,33 @@
 <html>
 <head>
   <title>Create Group</title>
+  <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="assets/ico/favicon.ico">
+
+    
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="navbar-fixed-top.css" rel="stylesheet">
+    <link href="welcome.css" rel="stylesheet">
+ 
   <style type="text/css">
+   .col{
+    color: #6600FF;
+    font-family: "Lucida Handwriting";
+  }
+  h1{ font-family: Magneto;
+  color:teal;}h1{ font-family: Magneto;
+  color:teal;}
   .align {text-align: center; color: blue;}
   .cen {text-align: center;}
-  h1{color:teal;} 
-  .id1{ color:goldenrod; font-size: 22px; } 
-  .id1:hover{ color:gold; }
-  .id2:hover{ color:indigo; } 
-  .id3{ color:olivedrab; font-size: 22px; } 
-  .id3:hover{ color:yellowgreen; } 
-  .id4{ color:mediumslateblue; font-size: 22px; } 
-  .id4:hover{ color:gold; } 
+  body {background-image:url("b1.jpg");}
   </style>
   <?php session_start();
 if(!$_SESSION['loggedin'])
@@ -21,44 +37,80 @@ exit;
 }
 ?>
 </head>
-<body style="background-color:lavender";>
-  <hr><hr>
-<p style="word-spacing: 3em;">  
-<a href="welcome.php" target=_top STYLE="text-decoration: none"><i><b><font class="id3">Home</font></b></i></a>
-<a href="create_group1.php" target=_top STYLE="text-decoration: none"><i><b><font class="id3">CreateGroup</font></b></i></a>
-<a href="" target=_top STYLE="text-decoration: none"><i><b><font class="id3">Contacts</font></b></i></a>
-<a href="" target=_top STYLE="text-decoration: none"><i><b><font class="id3">Profile</font></b></i></a>
-<a href="yourgroup.php" target=_top STYLE="text-decoration: none"><i><b><font class="id3">YourGroup</font></b></i></a>
-<a href="" target=_top STYLE="text-decoration: none"><i><b><font class="id3" style="word-spacing: 0.2em;">About us</font></b></i></a>
-<a href="" target=_top STYLE="text-decoration: none"><i><b><font class="id3">Help</font></b></i></a>
-<a href="login.php" target=_top STYLE="text-decoration: none"><i><b><font class="id3">Signout</font></b></i></a></p> <hr> <hr>
-<span class=align><h1><i>Create ur group</i></h1></span>
-<span class=cen><form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-<font class= "id4"></font>
-<input type="text" name="source"placeholder="Source: "><br><br>
-<font class= "id4"></font>
-<input type="text" name="destination"placeholder="Destination: "><br><br>
-<font class= "id4"></font>
-<input type="date" name="date"placeholder="Date: "><br><br>
-<font class= "id4"> </font>
-<input type="time" name="time"placeholder="Time:"><br><br>
-<font class= "id4"><abbr title="">Gender specific: </abbr>
-</font>
-<input type="radio" name="gender" value="F">only female
-<input type="radio" name="gender" value="M">only male
+<body style="background-color:lavender;">
+  <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Welcome <?php echo $_SESSION['userlogin']?> !</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="welcome1.php"><span class="glyphicon glyphicon-home"></span>  Home</a></li>
+            <li><a href="create_group.php"><span class="glyphicon glyphicon-list-alt"></span> Create Group</a></li>
+            <li><a href="#about"><span class="glyphicon glyphicon-phone-alt"></span>  Contacts</a></li>
+            <li><a href="#contact"><span class="glyphicon glyphicon-user"></span>  Profile</a></li>
+
+            
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-th-list"></span> <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Notifications</a></li>
+                <li><a href="http://www.facebook.com">Help</a></li>
+                
+                <li class="divider"></li>
+                <li class="dropdown-header">Account</li>
+                <li><a href="#">About Us</a></li>
+                
+              </ul>
+            </li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Help</a></li>
+            <li><a href="navbar-static-top/">About Us</a></li>
+            <li class="active"><a href="signout.php"><span class="glyphicon glyphicon-log-out"></span>  Sign-out</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+
+<h1 style="text-align:Center;"><b><font class="id2"><ins>Share Ur Fare</ins></font></b></h1>
+
+<marquee><b class=red>Disclaimer:</b><i>If any person in your group fails to come for the journey,then the site would not be responsible. Hence, user discretion is adviced.</i></marquee>
+
+
+<span><h3 class="col">&#160;&#160;&#160;&#160;&#160;&#160;Create ur group</h3></span>
+<form class="form-signin" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" role="form" method ="POST">
+
+<input type="text" class="form-control" placeholder="Source" name="source" required autofocus>
+        <input type="text" class="form-control" placeholder="Destination" name="destination" required>
+<input type="date"  onblur="(this.type='text')" onfocus="(this.type='date')" class="form-control" placeholder="Date Of Journey" name="date" required>
+<input type="time"  onfocus="(this.type='time')" onblur="(this.type='text')" class="form-control" placeholder="Time" name="time" required>
+<br>
+<font class= "gender"><abbr title="Any specific gender you want to share your fare with?">Gender specific: </abbr>
+</font><br>
+<input type="radio" name="gender" value="F">Only Female(for females only)<br>
+<input type="radio" name="gender" value="M">Only Male(for males only)<br>
 <input type="radio" name="gender" value="B">both<br><br>
-<font class= "id4"></font>
-<select name="vehicle">
-<option value="AUTO">AUTO</option>
-<option value="VIKRAM">VIKRAM</option>
+<select class="form-control" name="vehicle">
+<option value="AUTO">AUTO-RICKSHAW</option>
+<option value="VIKRAM">VIKRAM TEMPO</option>
 <option value="ANY">ANY</option>
 </select>
-<br><br>
-<font class= "id4"><abbr title="for whom you are booking"></abbr></font>
-<input type="number" name="number"placeholder="No. of people: "><br><br>
-<input type="number" name="limit"placeholder="Limit group to: "><br><br>
-<input type="submit" name="submit" value="create group">
-</form></sapn>
+
+
+<input type="number" name="number" class="form-control" placeholder="No. of people you are booking for ">
+<input type="number" name="limit" class="form-control" placeholder="Limit group to "><br><br>
+
+<button class="btn btn-lg btn-primary btn-block" type="submit"><span class="glyphicon glyphicon-plus"></span> &#160;Create Group
+</button>
+</form>
 <?php
   if($_SERVER["REQUEST_METHOD"]=="POST"){
   $con = @mysqli_connect('localhost','root','pcp10','iitk');
@@ -94,4 +146,7 @@ VALUES('$source','$destination','$date','$time','$gender','$vehicle','$number','
      @mysqli_close($con);}
       ?>
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 </body>
+</html>

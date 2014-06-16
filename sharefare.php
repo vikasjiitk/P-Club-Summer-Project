d60079deb9da523143c742a646e1df2975ffc2a0
pcp10 <2014pcp10@gmail.com>
2014-06-16 15:36:11 +0530

    Create sharefare.php

---
+++ sharefare.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="assets/ico/favicon.ico">
+
+    <title>Share Ur Fare</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="cover.css" rel="stylesheet">
+
+   
+<style>
+    h1{
+      font-family: Magneto;
+      color:#99FF33 ;
+      font-size: 60px;
+    }
+    </style>
+
+
+    <style type="text/css">
+    .we{
+      color: #33FFFF;
+    }
+.bg {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -5000;
+}
+</style>
+  </head>
+
+  <body>
+
+    <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          <div class="masthead">
+            <div class="inner">
+              
+              <ul class="nav masthead-nav">
+                <li class="active"><a href="login.php">Sign-In</a></li>
+                <li><a href="#">Help</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">About Us</a></li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="inner cover">
+            <h1>Share Ur Fare</h1>
+            <p class="lead we">Carpooling is a great way to save money, make new friends,<br> and reduce air pollution. And thats what <br><b>Share Ur Fare</b><br> precisely does !</p>
+            <p class="lead">
+              <a href="signup.php" class="btn btn-lg btn-default">Sign Up</a>
+            </p>
+          </div>
+ 
+
+        </div>
+
+      </div>
+
+    </div>
+    
+
+<div align="center"><img src="b2.jpg" class="bg"></div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/docs.min.js"></script>
+</body>
+</html>

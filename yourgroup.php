fca61d39033784263e44c4de37f0dfd4bfd17765
pcp10 <2014pcp10@gmail.com>
2014-06-18 16:13:00 +0530

    Update yourgroup.php

--- yourgroup.php
+++ yourgroup.php
@@ -1,8 +1,8 @@
-<!DOCTYPE html> 
-<html> 
+<!DOCTYPE html>
+<html>
 <head>
  <title>
- 	My Group
+  My Group
  </title>
  <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -28,33 +28,32 @@ exit;
 }
 
 ?>
-  <style type="text/css"> 
-   .col{
-    color: #6600FF;
-    font-family: "Lucida Handwriting";
-  }
-  h1{ font-family: Magneto;
-  color:teal;}
-  .bau{font-family: "Bradley Hand ITC";color: #330099;}
-  .align {text-align: center; color: blue;}
-  .cen {text-align: center;}
-  .aa{font-family: "Adobe Gothic Std B";color: #0033CC;font-size: 20px;}
-  body {background-image:url("b1.jpg");}
-  table,th,td
-  {
-  border:1px solid black;
-  border-collapse:collapse;
-  }
-  th,td
-  {
-  padding:5px;
-  }
-  h1{color:teal;} 
-   
-</style> 
-</head> 
+<style type="text/css">
+.col{
+color: #6600FF;
+font-family: "Lucida Handwriting";
+}
+h1{ font-family: Magneto;
+color:teal;}
+.bau{font-family: "Bradley Hand ITC";color: #330099;}
+.align {text-align: center; color: blue;}
+.cen {text-align: center;}
+.aa{font-family: "Adobe Gothic Std B";color: #0033CC;font-size: 20px;}
+body {background-image:url("b1.jpg");}
+table,th,td
+{
+border:1px solid black;
+border-collapse:collapse;
+}
+th,td
+{
+padding:5px;
+}
+h1{color:teal;}
+</style>
+</head>
 <body style="background-color:lavender;">
-  <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+<div class="navbar navbar-default navbar-fixed-top" role="navigation">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -71,46 +70,44 @@ exit;
             <li><a href="create_group.php"><span class="glyphicon glyphicon-list-alt"></span> Create Group</a></li>
            <li  class="active"><a href="yourgroup.php"><span class="glyphicon glyphicon-tasks"></span>  Your Group</a></li>
             
-            <li><a href="#contact"><span class="glyphicon glyphicon-user"></span>  Profile</a></li>
+            <li><a href="profile.php"><span class="glyphicon glyphicon-user"></span>  Profile</a></li>
 
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-th-list"></span> <b class="caret"></b></a>
               <ul class="dropdown-menu">
-                <li><a href="#">Profile</a></li>
+                
                 <li><a href="#">Notifications</a></li>
-                <li><a href="http://www.facebook.com">Help</a></li>
+                
                 
                 <li class="divider"></li>
                 <li class="dropdown-header">Account</li>
-                <li><a href="#">About Us</a></li>
+                <li><a href="#">Help</a></li>
                 
               </ul>
             </li>
           </ul>
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">Help</a></li>
-            <li><a href="navbar-static-top/">About Us</a></li>
-            <li class="active"><a href="signout.php"><span class="glyphicon glyphicon-log-out"></span>  Sign-out</a></li>
+            
+            <li><a href="aboutus.php">About Us</a></li>
+            <li><a href="signout.php"><span class="glyphicon glyphicon-log-out"></span>  Sign-out</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
     </div>
-<h1 style="text-align:Center;"><b><font class="id2"><ins>Share Ur Fare</ins></font></b></h1>
+    <h1 style="text-align:Center;"><b><font class="id2"><ins>Share Ur Fare</ins></font></b></h1>
 
 <marquee><b class=red>Disclaimer:</b><i>If any person in your group fails to come for the journey,then the site would not be responsible. Hence, user discretion is adviced.</i></marquee>
 
- 
-		 
-	<?php
-		require 'connect.inc.php';
-		$userid=@mysql_real_escape_string($_SESSION['loggedin']);
-     	$query="SELECT `key` FROM users WHERE `username`='$userid' ";
-     //	echo $userid;
-     	if(mysql_query($query))
-     	{
-     		//echo 'success';
-     		$run=mysql_query($query) or die(mysql_error());
-     		$row=mysql_fetch_assoc($run);
+<?php
+require 'connect.inc.php';
+$userid=@mysql_real_escape_string($_SESSION['loggedin']);
+      $query="SELECT `key` FROM users WHERE `username`='$userid' ";
+     // echo $userid;
+      if(mysql_query($query))
+      {
+      //echo 'success';
+      $run=mysql_query($query) or die(mysql_error());
+      $row=mysql_fetch_assoc($run);
         $group_id = $row["key"];
 
         if($group_id != 0)
@@ -122,14 +119,14 @@ exit;
             {
               $run2=mysql_query($query2) or die(mysql_error());
               $row2=mysql_fetch_assoc($run2);
-              echo '<table style="width:600px; text-align:center;" align="center" class="aa"> 
-              <tr style="background-color:#6699FF">
-              <th>Source</th> <th>Destination</th> <th>Date of Travel</th> <th>Time of Travel</th>
-              </tr>
-              <tr>
-              <td>'.$row2["source"].'</td> <td>'.$row2["destination"].'</td> <td>'.$row2["date"].'</td> <td>'.$row2["time"].
+              echo '<table style="width:600px; text-align:center;" align="center" class="aa">
+<tr style="background-color:#6699FF">
+<th>Source</th> <th>Destination</th> <th>Date of Travel</th> <th>Time of Travel</th>
+</tr>
+<tr>
+<td>'.$row2["source"].'</td> <td>'.$row2["destination"].'</td> <td>'.$row2["date"].'</td> <td>'.$row2["time"].
               '</td></tr>
-              </table>';
+</table>';
               $number=$row2['number'];
             }
 
@@ -137,22 +134,22 @@ exit;
             if(mysql_query($query3))
             {
               $run3=mysql_query($query3) or die(mysql_error());
-              echo '<table style="width:600px; text-align:center;" align="center" class="aa"> 
-              <tr style="background-color:#6699FF">
-              <th>S.no</th> <th>Name</th> <th>Email</th> <th>Phone no.</th> <th>Seats Booked</th>
-              </tr>';
+              echo '<table style="width:600px; text-align:center;" align="center" class="aa">
+<tr style="background-color:#6699FF">
+<th>S.no</th> <th>Name</th> <th>Email</th> <th>Phone no.</th> <th>Seats Booked</th>
+</tr>';
               $count=1;
               while($row3=mysql_fetch_assoc($run3))
               {
                 echo '<tr>
-                <td>'.$count.'</td><td>'.$row3["name"].'</td><td>'.$row3["username"].'@iitk.ac.in</td><td>'.$row3["phone"].'</td><td>'.$row3['book_no'].'</td></tr>';
+<td>'.$count.'</td><td>'.$row3["name"].'</td><td>'.$row3["username"].'@iitk.ac.in</td><td>'.$row3["phone"].'</td><td>'.$row3['book_no'].'</td></tr>';
                 $count++;
                 if($row3['username']==$userid)
                 {
                   $group=$row3['key'];
                   $book_no=$row3['book_no'];
                 }
-              } 
+              }
               echo '</table>';
               echo "<span class=align><form action='leave_group.php' method='post'>" ."<input type='hidden' name='username' value='$userid'>".
               "<input type='hidden' name='group' value='$group'>"."<input type='hidden' name='number' value='$number'>".
@@ -165,8 +162,11 @@ exit;
 
         }
         else echo '<span class=bau> <br><br><h2 style="text-align:center; color:red; font-size:40px;"><b>NO GROUP CREATED OR JOINED</b></h2</span> ';
-     		
-     	}
-     	else echo '<br>failure';
-     	?>
-     </body>
+     
+      }
+      else echo '<br>failure';
+      ?>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>

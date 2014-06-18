106c5b5324d81e449dd35608c6046bc8bcb63714
pcp10 <2014pcp10@gmail.com>
2014-06-19 04:07:56 +0530

    Update confirm_group.php

--- confirm_group.php
+++ confirm_group.php
@@ -20,9 +20,11 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
       $book_no=$_POST['book_no'];
      $key=$_POST['group'];
     $sql="UPDATE users SET `key`= '$key',`book_no`='$book_no' WHERE `username`='$username'";
+    $query="INSERT INTO notification(`username`,`key`,`code`,`time`) VALUES('$username','$key',1,NOW())";
+    $query1="UPDATE users SET `notify`=`notify`+1 WHERE `key`='$key' and `username`!='$username'";
     $number=$_POST['number'];
     mysqli_query($con,"UPDATE groups SET `number`='$number' WHERE `key`=$key");
-    if(!mysqli_query($con,$sql))
+    if(!mysqli_query($con,$sql) || !mysqli_query($con,$query) || !mysqli_query($con,$query1))
     {die('Error: '.mysqli_error($con));}
   else{
     $message = "Group joined successfully!";

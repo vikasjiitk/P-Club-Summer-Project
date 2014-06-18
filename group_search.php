acfad448710b89951ff76bae50c3896d2cb69b07
pcp10 <2014pcp10@gmail.com>
2014-06-18 16:11:34 +0530

    Update group_search.php

--- group_search.php
+++ group_search.php
@@ -40,45 +40,48 @@ body {background-image:url("b1.jpg");}
 .red {color: red;}
 </style>
 </head>
-<body> 
+<body>
 <div class="navbar navbar-default navbar-fixed-top" role="navigation">
-<div class="container">
-<div class="navbar-header">
-<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-<span class="sr-only">Toggle navigation</span>
-<span class="icon-bar"></span>
-<span class="icon-bar"></span>
-<span class="icon-bar"></span>
-</button>
-<a class="navbar-brand" href="#">Welcome <?php echo $_SESSION['userlogin']?> !</a>
-</div>
-<div class="navbar-collapse collapse">
-<ul class="nav navbar-nav">
-<li class="active"><a href="welcome.php"><span class="glyphicon glyphicon-home"></span> Home</a></li>
-<li><a href="create_group.php"><span class="glyphicon glyphicon-list-alt"></span> Create Group</a></li>
-<li><a href="yourgroup.php"><span class="glyphicon glyphicon-tasks"></span> Your Group</a></li>
-<li><a href="#about"><span class="glyphicon glyphicon-phone-alt"></span> Contacts</a></li>
-<li><a href="profile.php"><span class="glyphicon glyphicon-user"></span> Profile</a></li>
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Welcome <?php echo $_SESSION['userlogin']?> !</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+             <li class="active"><a href="welcome.php"><span class="glyphicon glyphicon-home"></span>  Home</a></li>
+            <li><a href="create_group.php"><span class="glyphicon glyphicon-list-alt"></span> Create Group</a></li>
+           <li><a href="yourgroup.php"><span class="glyphicon glyphicon-tasks"></span>  Your Group</a></li>
+            
+            <li><a href="profile.php"><span class="glyphicon glyphicon-user"></span>  Profile</a></li>
 
-<li class="dropdown">
-<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-th-list"></span> <b class="caret"></b></a>
-<ul class="dropdown-menu">
-<li><a href="#">Notifications</a></li>
-<li><a href="http://www.facebook.com">Help</a></li>
-<li class="divider"></li>
-<li class="dropdown-header">Account</li>
-<li><a href="#">About Us</a></li>
-</ul>
-</li>
-</ul>
-<ul class="nav navbar-nav navbar-right">
-<li><a href="#">Help</a></li>
-<li><a href="navbar-static-top/">About Us</a></li>
-<li class="active"><a href="signout.php"><span class="glyphicon glyphicon-log-out"></span> Sign-out</a></li>
-</ul>
-</div><!--/.nav-collapse -->
-</div>
-</div>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-th-list"></span> <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                
+                <li><a href="#">Notifications</a></li>
+                
+                
+                <li class="divider"></li>
+                <li class="dropdown-header">Account</li>
+                <li><a href="#">Help</a></li>
+                
+              </ul>
+            </li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            
+            <li><a href="aboutus.php">About Us</a></li>
+            <li><a href="signout.php"><span class="glyphicon glyphicon-log-out"></span>Sign-out</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
 
 <h1 style="text-align:Center;"><b><font class="id2"><ins>Share Ur Fare</ins></font></b></h1>
 
@@ -87,24 +90,24 @@ body {background-image:url("b1.jpg");}
 
 
 
-        <center>  <h2 class="sub-header bau"><b>Available Groups</b></h2></center>
-          <div class="table-responsive">
-            <table class="table table-striped aa">
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Source</th>
-                  <th>Destination</th>
-                  <th>Date</th>
-                  <th>Time</th>
-                  <th>Gender</th>
-                  <th>Seats</th>
-                  <th>Available</th>
-                  <th>Vehicle</th>
-                  <th>Join?</th>
-                </tr>
-              </thead>
-          <tbody>
+<center> <h2 class="sub-header bau"><b>Available Groups</b></h2></center>
+<div class="table-responsive">
+<table class="table table-striped aa">
+<thead>
+<tr>
+<th>#</th>
+<th>Source</th>
+<th>Destination</th>
+<th>Date</th>
+<th>Time</th>
+<th>Gender</th>
+<th>Seats</th>
+<th>Available</th>
+<th>Vehicle</th>
+<th>Join?</th>
+</tr>
+</thead>
+<tbody>
 
 
 <?php
@@ -144,7 +147,7 @@ if($_SERVER["REQUEST_METHOD"]=='POST')
                   $difference=strtotime($row['date']);
                     else
              $difference=strtotime($row['date'])-strtotime($date);
-             $t=abs(strtotime($row['time'])-$time1+$difference);  
+             $t=abs(strtotime($row['time'])-$time1+$difference);
             $key=$row['key'];
             $sql2="UPDATE groups SET `time_diff`='$t' WHERE `key`='$key'";
             if(mysql_query($sql2))
@@ -158,18 +161,18 @@ if($_SERVER["REQUEST_METHOD"]=='POST')
           if($desti=="")
           {
             if($var==-1)
-            { 
+            {
              if($date=="")
               $sql3="SELECT * FROM groups ORDER BY time_diff";
             else
               $sql3="SELECT * FROM groups WHERE `date`='$date' ORDER BY time_diff";
-            } 
+            }
             else
             {
               if($date=="")
               $sql3="SELECT * FROM groups WHERE time_diff<='$var' ORDER BY time_diff";
               else
-                  $sql3="SELECT * FROM groups WHERE time_diff<='$var' AND `date`='$date' ORDER BY time_diff";        
+                  $sql3="SELECT * FROM groups WHERE time_diff<='$var' AND `date`='$date' ORDER BY time_diff";
             }
         }
         else
@@ -185,24 +188,24 @@ if($_SERVER["REQUEST_METHOD"]=='POST')
           else
           {
             if($date=="")
-              $sql3="SELECT * FROM groups WHERE  destination='$desti' and time_diff<='$var' ORDER BY time_diff";
+              $sql3="SELECT * FROM groups WHERE destination='$desti' and time_diff<='$var' ORDER BY time_diff";
             else
-            $sql3="SELECT * FROM groups WHERE  destination='$desti' and time_diff<='$var' and `date`='$date' ORDER BY time_diff";
+            $sql3="SELECT * FROM groups WHERE destination='$desti' and time_diff<='$var' and `date`='$date' ORDER BY time_diff";
                      
-          } 
+          }
         }
       }
         else{
             if($desti=="")
           {
             if($var==-1)
-            { 
+            {
               if($date=="")
               $sql3="SELECT * FROM groups WHERE source='$source' ORDER BY time_diff";
             else
             $sql3="SELECT * FROM groups WHERE source='$source' and `date`='$date' ORDER BY time_diff";
               
-            } 
+            }
             else
             {
              if($date=="")
@@ -242,10 +245,10 @@ if($_SERVER["REQUEST_METHOD"]=='POST')
            echo '<span class=my>';
            echo "SORRY! NO GROUPS AVAILABLE";
            echo '</span>';
-          } 
+          }
           while($row=mysql_fetch_assoc($query_run2))
           {
-            echo 
+            echo
                 "<tr>".
                   "<td>".($i+1)."</td>".
                   "<td>".$row['source']."</td>".
@@ -265,7 +268,7 @@ if($_SERVER["REQUEST_METHOD"]=='POST')
              echo "<form action='join_group.php' method='post'>" ."<input type='hidden' name='group' value='$key'>".
     "<input type='hidden' name='number' value='$number'>"."<input type='hidden' name='limit' value='$limit'>".
     "<input type='hidden' name='gender' value='$gender'>"."<input type='hidden' name='book_no' value='$book_no'>".
-    "<button type='button submit' class='btn btn-lg btn-default'  name='join_group' value='Join Group'>Join Group</button>"."</form>"."</td>".
+    "<button type='button submit' class='btn btn-lg btn-default' name='join_group' value='Join Group'>Join Group</button>"."</form>"."</td>".
                 "</tr>";
               $i++;
           }

30241cc2b54a15d57e06d7d8933d07ad4fe94290
pcp10 <2014pcp10@gmail.com>
2014-06-21 01:40:46 +0530

    Update post.php

--- post.php
+++ post.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
 if(isset($_SESSION['loggedin'])){
-$array=explode(" ",$_SESSION['loggedin']); 
   $text = $_POST['text'];
-    $t=$array[1];
-    $fp = fopen($t, 'a');
-    fwrite($fp, "<div class='msgln'>(".date("g:i A").") <b>".$array[0]."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
+    $group=$_POST['group'];
+    $fp = fopen($group, 'a');
+    $timestamp=time();
+    fwrite($fp, "<div class='msgln'>(".date("g:i A",$timestamp+12600).") <b>".$_SESSION['loggedin']."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
     fclose($fp);
 }
 ?>
